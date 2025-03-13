49f155243ba040704ebb0038216f00178f7ab3c9
Alessio Mallamaci <alessiomallamaci@gmail.com>
2025-03-13 14:53:27 +0100

    Aggiunta libreria di input e output

--- main.c
+++ main.c
@@ -2,88 +2,99 @@
 	\brief Numerical integration of a polynomial
 	\author Paolo Gastaldo
 
-	first version of this software; all the functions are in this file  
+	Details.
 */
 
-/*! \mainpage 
- *
- * \section intro_sec Introduction
- *
- * This is a software in C language for computing numerical integration * by adopting different techniques.
-   
-   Available techniques: Riemann sum (Rectangular Rule), Trapezoidal Rule.
- 
- *
- * \section install_sec Usage
- *
- * 
- *
- * etc...
- */
-
- 
 #include <stdio.h> 
 #include <stdlib.h> 
-#include <string.h>
-
-//libreria aggiunta a mano per isolare il main dalle funzioni matematiche
+#include "io.h"
+#include "structs.h"
 #include "mathf.h"
+
 
 /*! \brief the main function
 		
-	here we define the polynomial, set the integration range and set the number of equally spaced intervals in which the range should be divided 
+	here we read the configuration file to: set the polynomial, set the integration range and set the number of equally spaced intervals in which the range should be divided 
 	the points delimiting the intervals are defined and then the integration is finalized 
 	
 */
+
 int main() {
 
-	/* setting all the parameters */
-	float fcoeff[4] = {-10.0,1.0,0.0,2.0};	/* the polynomial */
-	float xmin = 0.0; 						/* the integration range */ 
-	float xmax= 5.0; 					
-	int intervals = 5000; 					/* number of equally spaced intervals */ 
+	int steps;
+	float xmin, xmax;
+	poly_s pf;
 	
-	
-	float integ1, integ2;
-	
-	int i; 
-	float in = xmin;
-	float gap = (xmax-xmin)/(float)intervals; //allocazione della memoria per l'array
+	float in;
+	float gap;
 	float* fvalues = NULL;
 	
-	/* this array will store the (steps+1) values of the polynomial that are delimiting the equally spaced intervals*/
-	fvalues = (float*)malloc(sizeof(float)*(intervals+1));
-	if (fvalues == NULL) { //controllo se ha funzionato l'allocazione
+	float integ1, integ2;
+	int i;
+	int rv;	
+	FILE* fptr = NULL;
+	
+	/* opening the configuration file */
+	char filePath[100]="config.txt";
+	fptr=OpenFile(filePath);
+	if (fptr == NULL) {
+		printf("\n Main - ");
+		printf("ERROR: Unable to open file %s\n",filePath);
+		exit(-1);
+	}
+	
+	/* reading the configuration file */
+	rv=ReadConfigFile(fptr,&pf,&xmin,&xmax,&steps);
+	if (rv == -1) {
+		printf("\n Main - ");
+		printf("ERROR: Unable to read configuration file \n");
+		exit(-1);
+	}
+	
+	/* closing the configuration file */
+	rv=CloseFile(fptr);
+	if (rv == -1) {
+		printf("\n Main - ");
+		printf("ERROR: Unable to close configuration file \n");
+		exit(-1);
+	}
+	
+		
+	in = xmin;
+	gap = (float)(xmax-xmin)/(float)steps;
+	
+	/* allocation of the array fvalues */
+	fvalues = (float*)malloc(sizeof(float)*(steps+1));
+	if (fvalues == NULL) {
 		printf("\nERROR: cannot allocate memory\n");
 		exit(-1);
 	}
 
-	/* to get the value of the polynomial at the different points that are delimiting the intervals */
-	for (i=0; i<=intervals; i++) {
-		fvalues[i] = Polynomial(fcoeff,4,in); //popola i valori nell'array step per step
+	/* fvalues stores the values delimiting the intervals */
+	for (i=0; i<=steps; i++) {
+		fvalues[i] = Polynomial(pf,in);
 		in += gap;
+		
 	}
 	
 	integ1= 0.; 
 	integ2= 0.;
 
-	/* to compute the integral according to the rectangular rule */
-	Rectangular(fvalues,intervals+1,gap,&integ1,&integ2);
+	/* integral according to the Rectangular rule */
+	Rectangular(fvalues,steps+1,gap,&integ1,&integ2);
 	printf("\nRectangular rule - The integral between %f and %f is in the interval: [%f,%f]\n", xmin,xmax,integ1,integ2);
 	
-	/* to compute the integral according to the trapezoidal rule */
-	integ1 = Trapezoidal(fvalues,intervals+1,gap);
+	/* integral according to the Trapezoidal rule */
+	integ1 = Trapezoidal(fvalues,steps+1,gap);
 	printf("\nTrapezoidal rule - The integral between %f and %f is : %f\n", xmin,xmax,integ1);
 	
-	/* deallocation */
+	/* memory deallocation: the array included in pf */
+	if (pf.coeffs != NULL)
+		free(pf.coeffs);
+	
+	/* memory deallocation: the array fvalues */
 	if (fvalues != NULL)
 		free(fvalues);
 
 	return 0;
 }
-
-/*
-	TESTING: problemi del programma, cambinado i valori di alcuni programmi durante la lezione:
-		- se facciamo intervalli con valori troppo alti ==> intervalli piccoli --> problemi di instabilità numerica
-		- aumentando il grado del pominomio ho un po' più problemi	
-*/
